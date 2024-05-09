7c0bbfcf71612f6ae498f0903723540ad8ebf009
younasbzu <121710202+younasbzu@users.noreply.github.com>
2024-05-09 22:14:25 +0500

    Initial
    Button

--- lib/widgets/round_button.dart
+++ lib/widgets/round_button.dart
@@ -21,8 +21,8 @@ class RoundButton extends StatelessWidget {
           borderRadius:BorderRadius.circular(10),
         ),
         child: Center(
-          child:loading? CircularProgressIndicator(strokeWidth: 4, color: Colors.white,):
-          Text(title, style: TextStyle(color: Colors.white),),
+          child:loading?const CircularProgressIndicator(strokeWidth: 4, color: Colors.white,):
+          Text(title, style: const TextStyle(color: Colors.white),),
         ),
 
       ),
