2552583713da81e0e8aee803ca3bd9e7917e19c0
younasbzu <121710202+younasbzu@users.noreply.github.com>
2024-05-12 21:49:24 +0500

    Initial

--- lib/Dashboard/dashboard_screen.dart
+++ lib/Dashboard/dashboard_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_bzu/SplashScreen/authentication/login_screen.dart';
+import 'package:my_bzu/Utils/utils.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,11 +11,24 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  final auth= FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard'),
+        actions: [
+          IconButton(onPressed: (){
+            auth.signOut().then((value){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>const LoginScreen()));
+            }).onError((error, stackTrace){
+              Utils().toastMessage(error.toString());
+            });
+          }, icon:const Icon(Icons.logout)),
+          const SizedBox(width: 10,)
+        ],
       )
     );
   }
--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,12 +1,27 @@
 
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_bzu/Dashboard/dashboard_screen.dart';
 import 'package:my_bzu/SplashScreen/authentication/login_screen.dart';
 
 class SplashServices{
 
+
   void isLogin(BuildContext context){
-    Timer( const Duration(seconds: 5), () =>  Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginScreen())));
+    final auth= FirebaseAuth.instance;
+    final user= auth.currentUser;
+
+    if(user!=null){
+      Timer( const Duration(seconds: 5), () =>  Navigator.push(context,
+          MaterialPageRoute(builder: (context)=>const DashboardScreen())));
+
+    }else{
+      Timer( const Duration(seconds: 5), () =>  Navigator.push(context,
+          MaterialPageRoute(builder: (context)=>const LoginScreen())));
+    }
+
+
 
   }
 
